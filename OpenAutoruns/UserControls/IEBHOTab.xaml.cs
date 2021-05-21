1f3d3607fff880d88736055a5bdbc861316d4cd7
lzh-sjtu <1084388051@qq.com>
2021-05-21 11:28:44 +0800

    BHOs: add Internet Explorer BHOs autorun lookup

---
+++ OpenAutoruns/UserControls/IEBHOTab.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+using OpenAutoruns.Utilities;
+
+namespace OpenAutoruns.UserControls
+{
+    /// <summary>
+    /// Interaction logic for IEBHOTab.xaml
+    /// </summary>
+    public partial class IEBHOTab : UserControl
+    {
+        ObservableCollection<BHO> BHORegs = new ObservableCollection<BHO>();
+
+        public IEBHOTab()
+        {
+            InitializeComponent();
+            BHO.SearchRegBHOs(BHO.BHORegEntries, ref BHORegs);
+            ItemList.ItemsSource = BHORegs;
+            var collectionView = (CollectionView)CollectionViewSource.GetDefaultView(ItemList.ItemsSource);
+            collectionView.GroupDescriptions.Clear();
+            collectionView.GroupDescriptions.Add(new PropertyGroupDescription("Path"));
+        }
+    }
+}
